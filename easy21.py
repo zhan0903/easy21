dd2aabf8548e37e6adba621cfc7de92f75d2bc95
zhan0903 <z.han0903@qq.com>
2017-11-28 14:42:45 +1100

    begin

--- easy21.py
+++ easy21.py
@@ -1,6 +1,13 @@
-def step(s,a):
+#rewrite code is necessary if you want to achieve a better performance. for test, to finish it is more important.
+
+
+def Step(s,a):
     return r
 
+#Based on article 5 on-policy first-visit MC Control algorithm
+def Monte-Carlo-Control():
+    #initialize
 
-def start():
-    
+
+
+def Start():
